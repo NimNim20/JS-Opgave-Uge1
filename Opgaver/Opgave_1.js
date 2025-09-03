21c6fb83ad4691a7308ea8490649c8e4820382da
Benjamin Vid Gard <bbvgard@gmail.com>
2025-09-03 18:06:42 +0200

    First commit

---
+++ Opgaver/Opgave_1.js
@@ -0,0 +1,11 @@
+// Return summen af tal fra en liste
+
+// sum([1, 2, 3]);
+
+// function sum(list) {
+//   let sum = 0;
+//   for (let i = 0; i < list.length; i++) {
+//     sum += list[i];
+//   }
+//   console.log("The sum is:", sum);
+// }
---
+++ Opgaver/Opgave_3.js
@@ -0,0 +1,14 @@
+//Return mængden af vokaler i en streng
+
+// countVowels("React er bedre end Angular");
+
+// function countVowels(str) {
+//   let count = 0;
+//   const vowels = "aeiouæøåAEIOUÆØÅ";
+//   for (let i = 0; i < str.length; i++) {
+//     if (vowels.includes(str[i])) {
+//       count++;
+//     }
+//   }
+//   console.log("Mængden af vokaler:", count);
+// }
